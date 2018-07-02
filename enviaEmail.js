5d61199a7c6497b1d5938f3ca699cba3066eda99
eliseunogueira <eliseu.nogueira@outlook.com>
2018-07-02 12:18:40 -0300

    aprovação melhorada

--- enviaEmail.js
+++ enviaEmail.js
@@ -1,33 +1,44 @@
-const jwt = require('jsonwebtoken');
-const nodemailer = require('nodemailer');
-require('dotenv').config();
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+const { certificado } = require("./models");
+require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  service: "Gmail",
   auth: {
     user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
-  },
+    pass: process.env.GMAIL_PASS
+  }
 });
 
 const enviaEmail = async (certificadoId, email) => {
   try {
+    const conteudo = await certificado.findById(certificadoId);
     const emailToken = jwt.sign(
       {
-        certificadoId: certificadoId,
+        certificadoId: certificadoId
       },
       process.env.CERT_SECRET,
       {
-        expiresIn: '1d',
-      },
+        expiresIn: "1d"
+      }
     );
 
     const urlap = `http://localhost:8081/aprovado/${emailToken}`;
     const urlrp = `http://localhost:8081/reprovado/${emailToken}`;
     await transporter.sendMail({
       to: email,
-      subject: 'Confirm Email',
-      html: `<a href="${urlap}">Aprovado</a><br/>|<a href="${urlrp}">Reprovado</a>`,
+      subject: `Certificado${conteudo.id}`,
+      html: `
+      <b>Padrão do Equipamento:</b> ${conteudo.padrao}
+      <br/>
+      Observação: ${conteudo.observacao}
+      <br/>
+      <b>Data Realizada:</b> ${conteudo.data}
+      <br/>
+      <b>Ambiente:</b> ${conteudo.ambiente}
+      <br/>
+      <a href="${urlap}">Aprovado</a><br/>|<a href="${urlrp}">Reprovado</a>`
     });
   } catch (e) {
     throw new Error(e.message);
